4f5bfd9ea9cc53b81a58f5c12c72893b279441d6
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2021-05-16 11:31:39 +0300

    file upload

--- client/src/components/Preview/index.js
+++ client/src/components/Preview/index.js
@@ -1,7 +1,7 @@
 export const Preview = ({files}) => {
     if (!files.length) {
         return null
-    };
+    }
 
     return files.map((file) => <img style={{maxWidth: '200px'}} src={`//localhost:8000/${file.filename}`} alt={file.originalname}/>);
-}
+};
