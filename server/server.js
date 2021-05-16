83a6f88201d49ad8db6223e5be0916be42c711c3
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2021-05-16 11:34:17 +0300

    master branch

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const multer = require('multer');
 const cors = require('cors');
 
 const app = express();
@@ -7,26 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.static('public'));
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname)
-    }
-});
-
-const upload = multer({storage}).array('file');
-
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
-            return res.status(500).json(err)
-        }
-
-        return res.status(200).send(req.files)
-    })
-});
+app.post('/demo', (req, res) => res.status(200).send({demo: true}));
 
 app.listen(8000, () => {
     console.log('App is running on port 8000')
