e6e3877a2df7e19685134f69053d7c1ac410998a
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2021-06-27 23:08:47 +0300

    react performance tricks

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
+import {useState } from 'react'
 import './App.css';
+
+const list = new Array(20).fill(0).map(() => `Item - ${Math.random()}`);
+
+const List = ({list}) => {
+    const [filer, setFilter] = useState('');
+    const filteredElements = list.filter((item) => item.includes(filer));
+
+    return (
+        <>
+            <input type="text" placeholder="Filter" onChange={(e) => setFilter(e.target.value)}/>
+
+            <ul>
+                {filteredElements.map((item) => <li key={item}>{item}</li>)}
+            </ul>
+        </>
+    )
+};
+
+const Clicker = ({children}) => {
+    const [n, setN] = useState(0);
+
+    return (
+        <div data-count={n}>
+            {children}
+            <p>Clicked {n} times</p>
+            <button onClick={() => setN(n + 1)}>Click me</button>
+        </div>
+    )
+};
 
 function App() {
   return (
     <div className="App">
-      App
+        <Clicker>
+            <List list={list}/>
+        </Clicker>
     </div>
   );
 }
