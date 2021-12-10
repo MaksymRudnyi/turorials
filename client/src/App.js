91bef39e1f7ad1d0b9843fb7b7e6b27fa5b33b34
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2021-12-10 11:04:52 +0200

    setup moxios tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import axios from 'axios';
+import React, { useState, useEffect} from 'react';
 
 function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('/user?ID=12345')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                setUsers(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log('my error');
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }, [])
+
   return (
     <div className="App">
       App
+        <ul>
+        {
+            users.map((user) => (
+                <li>
+                    {user.name}
+                </li>
+            ))
+        }
+        </ul>
     </div>
   );
 }
