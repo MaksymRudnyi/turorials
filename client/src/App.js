0b20ba8efd164b6f9cdecff606cabc77d6ca8720
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2022-01-04 13:01:47 +0200

    ssr demo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      App
+      App test
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
