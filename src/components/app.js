66bd992cbfdb8f48d7686fb4643fc6a4dba4bdd0
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2022-01-07 21:19:28 +0200

    CSR

---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+export const App = () => {
+    console.log('app');
+    return <div className={styles.body}>App</div>
+};
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require( 'path' );
+const HTMLWebpackPlugin = require( 'html-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+
+/*-------------------------------------------------*/
+
+module.exports = {
+
+    //  webpack optimisation mode
+    mode: ( 'development' === process.env.NODE_ENV ? 'development' : 'production' ),
+
+    entry: [
+        './src/index.js', // react
+    ],
+
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'build/[name].js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [ 'babel-loader' ]
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+
+    plugins: [
+
+        new MiniCssExtractPlugin( {
+            filename: 'build/styles.css'
+        } ),
+
+        new HTMLWebpackPlugin( {
+            filename: 'index.html',
+            template: path.resolve( __dirname, 'src/index.html' ),
+            minify: false,
+        } ),
+
+        new CopyWebpackPlugin( {
+            patterns: [
+                {
+                    from: path.resolve( __dirname, 'src/assets' ),
+                    to: path.resolve( __dirname, 'dist/assets' )
+                }
+            ]
+        } ),
+    ],
+
+    resolve: {
+        extensions: [ '.js', '.jsx', '.scss' ],
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                default: false,
+                vendors: false,
+
+                vendor: {
+                    chunks: 'all',
+                    name: 'vendor',
+                    test: /node_modules/,
+                }
+            }
+        }
+    },
+
+    devServer: {
+        port: 8088,
+        historyApiFallback: true,
+    },
+
+    devtool: 'source-map'
+};
