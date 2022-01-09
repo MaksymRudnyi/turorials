259e19267b1f608d52a5f33f513c428663adaf33
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2022-01-09 15:55:58 +0200

    try to fetch data. Doesn't work with SSR

--- src/components/post.js
+++ src/components/post.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-export const Post = () => (
-    <div>
-        post
-    </div>
-)
+export const Post = () => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        return axios.get( 'https://jsonplaceholder.typicode.com/posts/' ).then( response => {
+            setData(response.data[0]);
+    } );
+    }, []);
+
+    return (
+        <div>
+            post
+            <h1>{data?.title}</h1>
+            <h2>{data?.body}</h2>
+        </div>
+    )
+};
