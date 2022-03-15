5650b0a9ad1c09b8d6b6c22383eca2b2574f98ed
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2022-03-15 21:25:31 +0200

    different types

--- server/server.js
+++ server/server.js
@@ -5,11 +5,17 @@ const { buildSchema} = require('graphql');
 const schema = buildSchema(`
     type Query {
         hello: String
+        quoteOfTheDay: String
+        random: Float
+        rollThreeDice: [Int]
     }
 `);
 
 const root = {
-    hello: () => 'Hello world.'
+    hello: () => 'Hello world.',
+    quoteOfTheDay: () => Math.random() < .5 ? 'Take is easy' : 'Salvation lies',
+    random: Math.random,
+    rollThreeDice: () => [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6))
 };
 
 const app = express();
