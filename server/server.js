e6d67fc81ce4a0515059180bc9c5e6ee066b0f56
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2022-03-17 21:25:12 +0200

    simple mutation

--- server/server.js
+++ server/server.js
@@ -1,24 +1,44 @@
+const fs = require('fs');
+const path = require('path');
 const { ApolloServer } = require('apollo-server');
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
 // 2
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links
+    },
+
+    Mutation: {
+        post: (parent, args) => {
+            let idCount = links.length;
+            const link = {
+                id: `link-${idCount}`,
+                description: args.description,
+                url: args.url
+            };
+
+            links.push(link);
+            return link
+        }
     }
 }
 
 // 3
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
     resolvers,
-})
+});
 
 server
     .listen()
