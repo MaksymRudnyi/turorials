aaabd1b19e997f0ca3caeb1ba47422b0dda60d26
Maksym Rudnyi <max.prog.zz@gmail.com>
2022-10-20 11:18:34 +0300

    craco example

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useState } from 'react';
+
+const Baner = ({ text }) => (
+  <p>text is - {text}</p>
+)
 
 function App() {
+  const [isActiveBaner, setActiveBaner] = useState(true);
   return (
     <div className="App">
       App
+      <button onClick={() => setActiveBaner(!isActiveBaner)}>Change</button>
+      <br/>
+      <Baner display-if={isActiveBaner} text={'Hello world!'}/>
     </div>
   );
 }
