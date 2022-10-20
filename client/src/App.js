bcc00d468e5fa48078afcf937e82ee179a58aa10
Maksym Rudnyi <max.prog.zz@gmail.com>
2022-10-20 13:58:37 +0300

    react markdown example

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import ReactMarkdown from 'react-markdown'
+import MARKDOWN from './markdown.md';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import rehypeLodashTemplate from 'rehype-lodash-template';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    fetch(MARKDOWN)
+      .then((res) => res.text())
+      .then((md) => {
+        setMarkdown(md)
+      })
+  }, []);
+
+  const variables = {
+    version: '1.1.1'
+  }
+
   return (
     <div className="App">
-      App
+      <ReactMarkdown children={markdown} rehypePlugins={[[rehypeLodashTemplate, { values: variables}]]} components={{
+        code: function code({node, inline, className, children, ...props}) {
+          const match = /language-(\w+)/.exec(className || '')
+          return !inline && match ? (
+            <SyntaxHighlighter
+              children={String(children).replace(/\n$/, '')}
+              style={dark}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            />
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          )
+        }
+      }}/>
     </div>
   );
 }
