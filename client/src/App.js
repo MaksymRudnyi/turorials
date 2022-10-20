892f8f8462672397b4d60b8f7c4bb333f1b7141c
Maksym Rudnyi <max.prog.zz@gmail.com>
2022-10-20 14:41:34 +0300

    loop in js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
 
+const Person = ({ name }) => (
+  <h1>Hello, {name}</h1>
+);
+
 function App() {
+  const dataInArray = [
+    {
+      id: 1,
+      name: 'John'
+    },
+    {
+      id: 2,
+      name: 'Kate'
+    },
+    {
+      id: 3,
+      name: 'Lisa'
+    },
+    {
+      id: 4,
+      name: 'Max'
+    }
+  ];
+
   return (
     <div className="App">
-      App
+      Loop inside React JSX
+      <br/>
+
+      {
+        dataInArray.map(({ name, id }) => <div key={id}><Person name={name} /></div>)
+      }
     </div>
   );
 }
