8ee8a6c028c51c883398c9a0b3ee5693e26f5bd8
Maksym Rudnyi <max.prog.zz@gmail.com>
2022-10-20 15:55:28 +0300

    storybook babel config

---
+++ client/src/stories/Button.stories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Button } from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Component',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    withTitle: true
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+  withTitle: true
+};
