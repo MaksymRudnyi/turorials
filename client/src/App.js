51c913d47ab171b0a803df2f5cf176def4b09fe2
Maksym Rudnyi <max.prog.zz@gmail.com>
2022-10-21 14:55:38 +0300

    lottie files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import Lottie from 'react-lottie';
+import loader from './loader.json';
+import { useState } from 'react';
 
 function App() {
+  const [isPaused, setIsPaused] = useState(false);
+  const [isStopped, setIsStopped] = useState(false);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loader,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
   return (
     <div className="App">
-      App
+      <h1>Lottie files</h1>
+
+      <Lottie options={defaultOptions}
+              width={300}
+              isPaused={isPaused}
+              isStopped={isStopped}
+      />
+
+      <br/>
+      <button onClick={() => setIsStopped(!isStopped)}>Stop</button>
+      <button onClick={() => setIsPaused(!isPaused)}>Run</button>
     </div>
   );
 }
