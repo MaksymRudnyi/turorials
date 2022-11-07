a2eabfe71e6e0abe4ce279d491e6cdac8a044d4c
Maksym Rudnyi <max.prog.zz@gmail.com>
2022-11-07 15:16:54 +0200

    slider from the package

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { CircleSlider } from "react-circle-slider";
 
 function App() {
+  const value = 300;
+  const handleChange = (value) => {
+    console.log('value: ', value);
+  }
+
   return (
     <div className="App">
-      App
+      <CircleSlider value={value}
+                    knobRadius={22}
+                    circleWidth={10}
+                    progressWidth={12}
+                    max={850}
+                    min={300}
+                    size={202}
+                    showPercentage={true}
+                    onChange={handleChange} />
     </div>
   );
 }
