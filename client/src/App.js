460f3a1af8a51195ad19ef0e6dbaa35d391de437
Maksym Rudnyi <max.prog.zz@gmail.com>
2023-01-26 18:32:06 +0200

    react bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Slider from './slider';
+import slides from './mock.json'
 
 function App() {
   return (
     <div className="App">
-      App
+      <Slider slides={slides}/>
     </div>
   );
 }
---
+++ client/src/slider.js
@@ -0,0 +1,35 @@
+import Carousel from 'react-bootstrap/Carousel';
+import React, { useState } from 'react';
+import './slider.css';
+
+function Slider({ slides }) {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    console.log('selected index: ', selectedIndex)
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index}
+              onSelect={handleSelect}
+              nextIcon={<span aria-hidden="true" className="carousel-control-next-icon changed" />}
+    >
+      {slides.map((slide) => (
+        <Carousel.Item key={slide.image} interval={slide.interval}>
+          <img
+            className="d-block w-100"
+            src={slide.image}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>{slide.title}</h3>
+            <p>{slide.subTitle}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+}
+
+export default Slider;
