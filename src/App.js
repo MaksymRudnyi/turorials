71dcdc8be07c034f7ed2bf0db79adb1f3462eea7
Maksym Rudnyi <max.prog.zz@gmail.com>
2023-02-22 22:01:12 +0200

    react-app-rewired

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [show, setShow] = useState(true);
   return (
     <div className="App">
       App
+
+      <button onClick={() => setShow(!show)}>Show/hide</button>
+
+      <h1 display-if={show}>Show this code</h1>
     </div>
   );
 }
