d5d53d26aa70e1170277be86e2008f78f4429420
Maksym Rudnyi <max.prog.zz@gmail.com>
2023-02-23 15:54:29 +0200

    react slider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import ReactSlider from 'react-slider';
+
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue ] = useState([])
   return (
     <div className="App">
-      App
+      <ReactSlider
+        className="horizontal-slider"
+        thumbClassName="example-thumb"
+        trackClassName="example-track"
+        defaultValue={[1950, 2023]}
+        max={2023}
+        min={1950}
+        renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+        onChange={(value, index) => setValue(value)}
+      />
+
+      <hr/>
+
+      Start: {value[0]}
+      <br/>
+      End: {value[1]}
     </div>
   );
 }
